f1809488133d3b0b20bca8f09653dd267ce6ef8b
GuilhermeBiscalchin <desenvolvendogame@gmail.com>
2021-10-07 18:16:21 -0300

    modulos

---
+++ JavaScript/modulos/funcoes.mjs
@@ -0,0 +1,17 @@
+function mostraIdade(nome,idade){
+    return `A idade de ${nome} é ${idade}`
+}
+
+function mostraCidade(cidade){
+    return `Nascido em ${cidade}`
+}
+
+function mostraHobby(hobby){
+    return `E seu hobby é ${hobby}`
+}
+
+export{
+    mostraIdade,
+    mostraCidade,
+    mostraHobby
+}
---
+++ JavaScript/modulos/main.mjs
@@ -0,0 +1,7 @@
+import { mostraIdade,mostraCidade,mostraHobby } from "./funcoes.mjs";
+
+
+
+console.log(mostraIdade('Guilherme',30))
+console.log(mostraCidade("Bauru")),
+console.log(mostraHobby('Jiu-Jitsu'))
