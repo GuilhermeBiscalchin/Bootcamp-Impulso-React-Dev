525a370e65230d369c2a3c3ec7d1f7bb12ef220a
GuilhermeBiscalchin <desenvolvendogame@gmail.com>
2021-10-13 22:46:28 -0300

    webpack

---
+++ react/guilherme __webpack/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from './Button'
+
+const App = () => {
+    return(
+        <div>
+            <p>Digital Innovation One</p>
+            <p>Bem vindo ao webpack</p>
+            <Button />
+        </div>
+    )
+}
+
+export default App
